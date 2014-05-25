e627515c23e9f330516cc03b31188cd23b1b5d4f
Kenneth Lee <kennethkl@gmail.com>
2014-05-25 13:35:02 -0400

    redefined julius triggers

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var julius = require('julius-net')({host: process.env.JULIUS_HOST || 'localhost', port: process.env.JULIUS_PORT || 10500});
+var Julius = require('julius-net'),
+    julius = new Julius({host: process.env.JULIUS_HOST || 'localhost', port: process.env.JULIUS_PORT || 10500});
 
 julius.on('recognitionSuccess', function(rec) {
     console.log(rec);
+    console.log(rec[0].words);
 });
